da4e36becbc1d97e00a05252b7680cfe5131182b
birkorff <rickelsonbresume96@gmail.com>
2022-06-19 18:41:57 -0400

    edddd

--- functions/db.php
+++ functions/db.php
@@ -1,4 +1,7 @@
 <?php
-    $conn=mysqli_connect("localhost","root","root","onematch");
-    return $conn;
+     function conectar(){
+        $conn=mysqli_connect("localhost","root","root","onematch");
+        return $conn;
+     }
+   
 ?>
