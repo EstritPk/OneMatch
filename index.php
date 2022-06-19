bd62f87da03ce5178e858da82de1906947ea1407
birkorff <rickelsonbresume96@gmail.com>
2022-06-19 00:17:38 -0400

    lsssssssss

--- index.php
+++ index.php
@@ -5,50 +5,71 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
     <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
-    <link rel="stylesheet" href="css/estilos.css">
-    <script src="js/validaciones.js"></script>
+    <link rel="stylesheet" href="css/estlog.css">
+    <title>Login</title>
 </head>
 
 <body>
+    <section class="h-100 gradient-form bg-dark">
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-xl-10 ">
+                    <div class="card  shadow-lg rounded-3 text-white">
+                        <div class="row g-0">
+                            <div class="col-lg-6 bg-dark ">
+                                <div class="card-body p-md-5 mx-md-4">
 
-    <div class="container  ">
+                                    <div class="text-center">
+                                        <img src="images/My project.png" style="width: 265px;" alt="logo">
 
-        <div class="row justify-content-center">
-            <div class="col-md-7 col-lg-5 ">
+                                    </div>
 
-            <!-- DEBO HACER CRUD -->
+                                    <form action="" class="needs-validation formulario" method="POST" novalidate>
 
-                <div class="login-wrap p-4 p-md-5 card posi center">
 
-                    <form name="formlog" action="#" class="signin-form formulario needs-validation"  method="POST" novalidate>
-                        <div class="form-group mt-3 contenedor-inputs">
-                            <label class="form-control-placeholder" name="usern" for="username">Username</label>
-                            <input type="text" class="form-control" required>
+                                        <div class="form-outline form-white mb-4">
+                                            <input type="text" id="form2Example11" class="form-control" placeholder="ingresa tu rut o user name" require />
 
+                                        </div>
+
+                                        <div class="form-outline form-white mb-4">
+                                            <input type="password" id="form2Example22" class="form-control" require />
+
+                                        </div>
+
+                                        <div class="text-center pt-1 mb-5 pb-1">
+                                            <button type="submit" class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" ">Ingresar</button>
+                                            <a class=" text-danger m-0" href="#!">Olvidaste tu contraseña?</a>
+                                        </div>
+                                    </form>
+                                    <div class="d-flex align-items-center justify-content-center pb-4">
+                                        <p class="mb-0 me-2 text-white">No tienes una cuenta?</p>
+                                        <button type="button" class="btn btn-outline-danger">Crear cuenta</button>
+                                    </div>
+
+
+
+                                </div>
+                            </div>
+                            <div class="col-lg-6 d-flex align-items-center gradient-custom-2">
+                                <div class="text-white px-3 py-4 p-md-5 mx-md-4">
+                                    <h4 class="mb-4"></h4>
+                                    <p class="lg mb-0">"Lo más importante del deporte no es ganar, sino participar, porque lo esencial en la vida no es el éxito, sino esforzarse por conseguirlo".
+                                        Barón Pierre de Coubertin.</p>
+                                </div>
+                            </div>
                         </div>
-                        <div class="form-group contenedor-inputs">
-                            <label class="form-control-placeholder" name="claveuser" for="password">Password</label>
-                            <input id="password-field" type="password" class="form-control" required>
-                            <span toggle="#password-field" class="fa fa-fw fa-eye field-icon toggle-password"></span>
-                        </div>
-                        <div class="form-group">
-                            <button type="submit" onclick="validarLoginU()" class="form-control btn  rounded submit px-3">Ingresar</button>
-                        </div>
-                    </form>
-
-                    <p class="text-center">No tiene cuenta? <a data-toggle="tab" href="#signup">Crear Cuenta</a></p>
-                    
+                    </div>
                 </div>
-
             </div>
         </div>
-    </div>
+    </section>
+
+
 
 
     <script src="js/validaciones.js"></script>
-
     <script src="js/bootstrap.bundle.min.js"></script>
 </body>
 
