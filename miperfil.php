8a39dfba8871525fc17f908669859061c69f1098
birkorff <rickelsonbresume96@gmail.com>
2022-06-23 18:48:11 -0400

    all

--- miperfil.php
+++ miperfil.php
@@ -1,11 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
+<?php
+
+include "functions/db.php";
+
+
+$sqlpro = "SELECT * FROM usuarios where rut_usuario=34562";
+$resultpro = mysqli_query(conectar(), $sqlpro);
+$datos = mysqli_fetch_array($resultpro);
+
+
+?>
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Perfil Usuario</title>
+    <link rel="stylesheet" href="css/owl.carousel.min.css">
+    <link rel="stylesheet" href="css/owl.theme.default.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
     <script src="https://kit.fontawesome.com/b8c0c93cb3.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/miperfil.css">
@@ -16,63 +30,29 @@
 
 
 
-   
-        <!-- Top navbar -->
-        <div class="pt-5">
-            <?php
-            include "includes/header.php";
-            ?>
-        </div>
-        <!-- User -->
-        <ul class="navbar-nav align-items-center d-none d-md-flex">
-            <li class="nav-item dropdown">
 
-                <div class="dropdown-menu dropdown-menu-arrow dropdown-menu-right">
-                    <div class=" dropdown-header noti-title">
-                        <h6 class="text-overflow m-0">Bienvenido!</h6>
-                    </div>
-                    <a href="../examples/profile.html" class="dropdown-item">
-                        <i class="ni ni-single-02"></i>
-                        <span>My profile</span>
-                    </a>
-                    <a href="../examples/profile.html" class="dropdown-item">
-                        <i class="ni ni-settings-gear-65"></i>
-                        <span>Settings</span>
-                    </a>
-                    <a href="../examples/profile.html" class="dropdown-item">
-                        <i class="ni ni-calendar-grid-58"></i>
-                        <span>Activity</span>
-                    </a>
-                    <a href="../examples/profile.html" class="dropdown-item">
-                        <i class="ni ni-support-16"></i>
-                        <span>Support</span>
-                    </a>
-                    <div class="dropdown-divider"></div>
-                    <a href="#!" class="dropdown-item">
-                        <i class="ni ni-user-run"></i>
-                        <span>Logout</span>
-                    </a>
-                </div>
-            </li>
-        </ul>
+
+    <div class="pt-3">
+        <?php
+        include "includes/header.php";
+        ?>
     </div>
-    </nav>
-    <!-- Header -->
-    <div class="header pb-8 pt-5 pt-lg-8 d-flex align-items-center" style="min-height: 600px; background-image: url(https://raw.githubusercontent.com/creativetimofficial/argon-dashboard/gh-pages/assets-old/img/theme/profile-cover.jpg); background-size: cover; background-position: center top;">
-        <!-- Mask -->
+
+    <div class="header pb-8 pt-5 pt-lg-8 d-flex bg-info align-items-center" style="min-height: 600px; background-image: url(images/dep2.jpeg); background-size: cover; background-position: center top;">
+
         <span class="mask bg-gradient-default opacity-8"></span>
-        <!-- Header container -->
+
         <div class="container-fluid d-flex align-items-center">
             <div class="row">
-                <div class="col-lg-7 col-md-10">
-                    <h1 class="display-2 text-white">Hello Jesse</h1>
-                    <p class="text-white mt-0 mb-5">This is your profile page. You can see the progress you've made with your work and manage your projects or assigned tasks</p>
-                    <a href="#!" class="btn btn-info">Edit profile</a>
+                <div class="col-lg-12 col-md-10">
+                    <h1 class=" text-white"><?php echo $datos['nombre_usuario'], " ", $datos['apellido_p_usuario']; ?></h1>
+                    <p class="text-white mt-0 mb-5">estarrrrrrr</p>
+                    <a href="#!" class="btn btn-info">Editar tu prerfil</a>
                 </div>
             </div>
         </div>
     </div>
-    <!-- Page content -->
+
     <div class="container-fluid mt--7">
         <div class="row">
             <div class="col-xl-4 order-xl-2 mb-5 mb-xl-0">
@@ -81,52 +61,41 @@
                         <div class="col-lg-3 order-lg-2">
                             <div class="card-profile-image">
                                 <a href="#">
-                                    <img src="https://demos.creative-tim.com/argon-dashboard/assets-old/img/theme/team-4.jpg" class="rounded-circle">
+                                    <img src="images/perfil.jpg" class="rounded-circle">
                                 </a>
                             </div>
                         </div>
                     </div>
                     <div class="card-header text-center border-0 pt-8 pt-md-4 pb-0 pb-md-4">
-                        <div class="d-flex justify-content-between">
-                            <a href="#" class="btn btn-sm btn-info mr-4">Connect</a>
-                            <a href="#" class="btn btn-sm btn-default float-right">Message</a>
-                        </div>
+
                     </div>
                     <div class="card-body pt-0 pt-md-4">
                         <div class="row">
                             <div class="col">
                                 <div class="card-profile-stats d-flex justify-content-center mt-md-5">
-                                    <div>
-                                        <span class="heading">22</span>
-                                        <span class="description">Friends</span>
-                                    </div>
+
                                     <div>
                                         <span class="heading">10</span>
-                                        <span class="description">Photos</span>
+                                        <span class="description">Equipos</span>
                                     </div>
-                                    <div>
-                                        <span class="heading">89</span>
-                                        <span class="description">Comments</span>
-                                    </div>
+
                                 </div>
                             </div>
                         </div>
                         <div class="text-center">
                             <h3>
-                                Jessica Jones<span class="font-weight-light">, 27</span>
+                                <?php echo $datos['nombre_usuario'], " ", $datos['apellido_p_usuario']; ?><span class="font-weight-light">,
+                                    <?php echo $datos['edad_usuario']; ?></span>
                             </h3>
                             <div class="h5 font-weight-300">
-                                <i class="ni location_pin mr-2"></i>Bucharest, Romania
+                                <i class="ni location_pin mr-2"></i>Chile,Coquimbo
                             </div>
                             <div class="h5 mt-4">
-                                <i class="ni business_briefcase-24 mr-2"></i>Solution Manager - Creative Tim Officer
+                                <i class="ni business_briefcase-24 mr-2"></i>Mi Descripcion
                             </div>
-                            <div>
-                                <i class="ni education_hat mr-2"></i>University of Computer Science
-                            </div>
-                            <hr class="my-4">
-                            <p>Ryan — the name taken by Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs and records all of his own music.</p>
-                            <a href="#">Show more</a>
+                          
+                            <p><?php echo $datos['descripcion_usuario']; ?></p>
+                            
                         </div>
                     </div>
                 </div>
@@ -136,104 +105,90 @@
                     <div class="card-header bg-white border-0">
                         <div class="row align-items-center">
                             <div class="col-8">
-                                <h3 class="mb-0">My account</h3>
+                                <h3 class="mb-0">MI Cuentas</h3>
                             </div>
                             <div class="col-4 text-right">
-                                <a href="#!" class="btn btn-sm btn-primary">Settings</a>
+                                <a href="#!" class="btn btn-sm btn-primary">Mis Reservas</a>
                             </div>
                         </div>
                     </div>
                     <div class="card-body">
-                        <form>
-                            <h6 class="heading-small text-muted mb-4">User information</h6>
-                            <div class="pl-lg-4">
-                                <div class="row">
-                                    <div class="col-lg-6">
-                                        <div class="form-group focused">
-                                            <label class="form-control-label" for="input-username">Username</label>
-                                            <input type="text" id="input-username" class="form-control form-control-alternative" readonly placeholder="Username" value="lucky.jesse">
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-6">
-                                        <div class="form-group">
-                                            <label class="form-control-label" for="input-email">Email address</label>
-                                            <input type="email" id="input-email" class="form-control form-control-alternative" readonly placeholder="jesse@example.com">
-                                        </div>
+
+                        <h4 class="  text-light mb-4">Datos del Usuario</h4>
+                        <div class="pl-lg-4">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <div class="form-group focused">
+                                        <label class="form-control-label" for="input-username">Nobre de usuario</label>
+                                        <input type="text" id="input-username" class="form-control form-control-alternative" readonly value="<?php echo $datos['edad_usuario']; ?>">
                                     </div>
                                 </div>
-                                <div class="row">
-                                    <div class="col-lg-6">
-                                        <div class="form-group focused">
-                                            <label class="form-control-label" for="input-first-name">First name</label>
-                                            <input type="text" id="input-first-name" class="form-control form-control-alternative" readonly placeholder="First name" value="Lucky">
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-6">
-                                        <div class="form-group focused">
-                                            <label class="form-control-label" for="input-last-name">Last name</label>
-                                            <input type="text" id="input-last-name" class="form-control form-control-alternative" readonly placeholder="Last name" value="Jesse">
-                                        </div>
+                                <div class="col-lg-6">
+                                    <div class="form-group">
+                                        <label class="form-control-label" for="input-email">Correo</label>
+                                        <input type="email" id="input-email" class="form-control form-control-alternative" readonly placeholder="<?php echo $datos['edad_usuario']; ?>">
                                     </div>
                                 </div>
                             </div>
-                            <hr class="my-4">
-                            <!-- Address -->
-                            <h6 class="heading-small text-muted mb-4">Contact information</h6>
-                            <div class="pl-lg-4">
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="form-group focused">
-                                            <label class="form-control-label" for="input-address">Address</label>
-                                            <input id="input-address" class="form-control form-control-alternative" readonly placeholder="Home Address" value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09" type="text">
-                                        </div>
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <div class="form-group focused">
+                                        <label class="form-control-label" for="input-first-name">Nombre</label>
+                                        <input type="text" id="input-first-name" class="form-control form-control-alternative" readonly placeholder="First name" value="<?php echo $datos['edad_usuario']; ?>">
                                     </div>
                                 </div>
-                                <div class="row">
-                                    <div class="col-lg-4">
-                                        <div class="form-group focused">
-                                            <label class="form-control-label" for="input-city">City</label>
-                                            <input type="text" id="input-city" readonly class="form-control form-control-alternative" placeholder="City" value="New York">
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-4">
-                                        <div class="form-group focused">
-                                            <label class="form-control-label" for="input-country">Country</label>
-                                            <input type="text" id="input-country" readonly class="form-control form-control-alternative" placeholder="Country" value="United States">
-                                        </div>
-                                    </div>
-                                    <div class="col-lg-4">
-                                        <div class="form-group">
-                                            <label class="form-control-label" for="input-country">Postal code</label>
-                                            <input type="number" id="input-postal-code" readonly class="form-control form-control-alternative" placeholder="Postal code">
-                                        </div>
+                                <div class="col-lg-6">
+                                    <div class="form-group focused">
+                                        <label class="form-control-label" for="">Apellidos</label>
+                                        <input type="text"  class="form-control form-control-alternative" readonly placeholder="Last name" value="<?php echo $datos['edad_usuario']; ?>">
                                     </div>
                                 </div>
                             </div>
-                            <hr class="my-4">
-                            <!-- Description -->
-                            <h6 class="heading-small text-muted mb-4">About me</h6>
-                            <div class="pl-lg-4">
-                                <div class="form-group focused">
-                                    <label>About Me</label>
-                                    <textarea rows="4" class="form-control form-control-alternative" readonly placeholder="A few words about you ...">A beautiful Dashboard for Bootstrap 4. It is Free and Open Source.</textarea>
+                        </div>
+                        <hr class="my-4">
+                        <!-- Address -->
+                        <h4 class="text-light text-muted mb-4">Contact information</h4>
+                        <div class="pl-lg-4">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <div class="form-group focused">
+                                        <label class="form-control-label" for="input-address">Direccion</label>
+                                        <input id="input-address" class="form-control form-control-alternative" readonly placeholder="Home Address" value="Bld Mihail Kogalniceanu, nr. 8 Bl 1, Sc 1, Ap 09" type="text">
+                                    </div>
                                 </div>
                             </div>
-                        </form>
+                            <div class="row">
+                                <div class="col-lg-4">
+                                    <div class="form-group focused">
+                                        <label class="form-control-label" for="input-city">Ciudad</label>
+                                        <input type="text" id="input-city" readonly class="form-control form-control-alternative" placeholder="City" value="New York">
+                                    </div>
+                                </div>
+                                <div class="col-lg-4">
+                                    <div class="form-group focused">
+                                        <label class="form-control-label" for="input-country">Pais</label>
+                                        <input type="text" id="input-country" readonly class="form-control form-control-alternative" placeholder="Country" value="United States">
+                                    </div>
+                                </div>
+                                <div class="col-lg-4">
+                                    <div class="form-group">
+                                        <label class="form-control-label" for="input-country">Telefono</label>
+                                        <input type="number" id="input-postal-code" readonly class="form-control form-control-alternative" placeholder="Postal code">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <hr class="my-4">
+                        <!-- Description -->
+
+
                     </div>
                 </div>
             </div>
         </div>
     </div>
     </div>
-    <footer class="footer">
-        <div class="row align-items-center justify-content-xl-between">
-            <div class="col-xl-6 m-auto text-center">
-                <div class="copyright">
-                    <p>Made with <a href="https://www.creative-tim.com/product/argon-dashboard" target="_blank">Argon Dashboard</a> by Creative Tim</p>
-                </div>
-            </div>
-        </div>
-    </footer>
+
 
 
 
@@ -253,9 +208,9 @@
     <script src="js/popper.js"></script>
 
     <script src="js/owl.carousel.min.js"></script>
-    
+
     <script src="js/main.js"></script>
-    <script src="js/validaciones.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
 </body>
 
 </html>
